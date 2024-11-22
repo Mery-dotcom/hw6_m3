fdd978d1bf1be1051c95ca07ba61b2a90c9e9c43
Mery-dotcom <meka-28list.ru>
2024-11-22 17:37:45 +0600

    change MainActivity

--- app/src/main/java/com/geeks/hw6_m3/MainActivity.kt
+++ app/src/main/java/com/geeks/hw6_m3/MainActivity.kt
@@ -5,10 +5,16 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.geeks.hw6_m3.databinding.ActivityMainBinding
+import com.geeks.hw6_m3.databinding.FragmentContinentsBinding
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container_view, FirstFragment ())
+            .commit()
     }
 }
